c0bce606d1af8c5bbdd9d077585fc15bd520f386
Jayant Issar <jayantissar23@gmail.com>
2024-02-27 17:24:09 +0530

    trying to fix image issue

--- frontend/allTexts.jsx
+++ frontend/allTexts.jsx
@@ -3,3 +3,4 @@
 
 export const aboutJayant = "I think maybe we can partner up. Hey there, I am Jayant Issar a 1st year undergrad student of Sri Venketshwara College Delhi University more than that a Software enginnering enthusiast who is striving to become a 100x engineer, thanks for visiting. Feel free to check out all the projects and other handles which are linked"
 export const photCredits = "Name-Jayant Issar Fullstack Devloper"
+export const photo = "../assets/jayant.jpg"
