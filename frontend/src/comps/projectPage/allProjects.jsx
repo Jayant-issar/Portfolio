593067547a0541879e19ef3e01ae7bac4d9c757a
Jayant Issar <jayantissar23@gmail.com>
2024-03-19 08:04:14 +0530

    added story vault youtube link

--- frontend/src/comps/projectPage/allProjects.jsx
+++ frontend/src/comps/projectPage/allProjects.jsx
@@ -9,7 +9,7 @@ export function AllProjects(){
     return(
         <div id="allProjectsParent" className=" mt-5 p-4">
             <ProjectCard about={aboutProjects.storyvault} img1="./story-valult/strory-vaultlan.PNG"
-            projectName={"Story-Vault"} github={"https://github.com/Jayant-issar/Story-Vault"} deployed={"https://story-vault-jayant.vercel.app/"} />
+            projectName={"Story-Vault"} github={"https://github.com/Jayant-issar/Story-Vault"} youtube={"https://youtu.be/yRZmWmoL1FA"} deployed={"https://story-vault-jayant.vercel.app/"} />
             <ProjectCard about={aboutProjects.swiftwallet} projectName={"Swift-Wallet"}
              img1="./swift-wallet/sw1.png" github={"https://github.com/Jayant-issar/swift-wallet"} youtube="https://youtu.be/BkX52nlOPQo?si=d0MVuSXh-gj9MuGH" />
             <ProjectCard about={aboutProjects.portfolio} img1="./portfolio-imgs/portfolio-hero.png"
